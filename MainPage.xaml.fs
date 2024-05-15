eca5e8863a95ab66d648132357605898b4da1d8c
webwarrior <reg@webwarrior.ws>
2024-05-15 12:51:38 +0200

    add some debug logging

--- MainPage.xaml.fs
+++ MainPage.xaml.fs
@@ -14,15 +14,17 @@ type MainPage() =
     let _ = base.LoadFromXaml(typeof<MainPage>)
     
     member this.OnCounterClicked(sender: obj, e: EventArgs) =
+        Android.Util.Log.Debug("Custom", "Go to second page button clicked") |> ignore
         MainThread.BeginInvokeOnMainThread(fun () ->
             let newPage = new SecondPage()
             NavigationPage.SetHasNavigationBar(newPage, true)
 
             let navPage = new NavigationPage(newPage)
 
+            Android.Util.Log.Debug("Custom", "Navigating to second page") |> ignore
             this.Navigation.InsertPageBefore(navPage, this)
             this.Navigation.PopAsync().ContinueWith((fun (t: Task) -> 
-                ()
+                Android.Util.Log.Debug("Custom" ,sprintf "Exception: %A" t.Exception) |> ignore
             ), TaskContinuationOptions.OnlyOnFaulted)
             |> ignore
         );
--- SecondPage.xaml.fs
+++ SecondPage.xaml.fs
@@ -14,6 +14,7 @@ type SecondPage() =
     let _ = base.LoadFromXaml(typeof<SecondPage>)
 
     member this.OnButtonClicked(sender: obj, e: EventArgs) =
+        Android.Util.Log.Debug("Custom", "Go to third page button clicked") |> ignore
         MainThread.BeginInvokeOnMainThread(fun () ->
             let newPage = new ThirdPage()
             NavigationPage.SetHasNavigationBar(newPage, true)
@@ -21,8 +22,9 @@ type SecondPage() =
             let navPage = new NavigationPage(newPage)
             NavigationPage.SetHasNavigationBar(navPage, true)
 
+            Android.Util.Log.Debug("Custom", "Navigating to third page") |> ignore
             this.Navigation.PushAsync(navPage).ContinueWith((fun (t: Task) -> 
-                ()
+                Android.Util.Log.Debug("Custom" ,sprintf "Exception: %A" t.Exception) |> ignore
             ), TaskContinuationOptions.OnlyOnFaulted)
             |> ignore
         )
